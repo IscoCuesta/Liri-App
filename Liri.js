30f238695ae4e0b4d68bcc04a81e5f4650cfd299
Isco <Iscocuesta@hotmail.com>
2019-05-17 14:54:11 -0500

    final version

--- Liri.js
+++ Liri.js
@@ -1,12 +1,16 @@
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var moment = require('moment');
+var fs = require("fs");
 
 require('dotenv').config();
 var keys = require("./keys.js");
 
+
 var command = process.argv[2];
 var request = process.argv[3];
+
+function run (){
 
 switch(command){
   case "concert-this":
@@ -17,7 +21,7 @@ switch(command){
         for( var i = 0; i<response.data.length; i++){
         console.log("Venue: " + response.data[i].venue.name);
         console.log("location: " + response.data[i].venue.city + ", " +response.data[i].venue.country);
-        console.log("date MM/DD/YYYY: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+        console.log("date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
         console.log("<------------------>");
         }
       }
@@ -27,14 +31,20 @@ switch(command){
   break;
   case "spotify-this-song":
 
-    var spotify = new Spotify(keys.spotify);
   
+    var spotify = new Spotify(keys.spotify);
+
     spotify.search({ type: 'track', query: request }, function(err, data) {
       if (err) {
         return console.log('Error occurred: ' + err);
       }
-    
-    console.log(data); 
+      for( var i = 0; i<5; i++){
+      console.log("Artist: "+data.tracks.items[i].artists[0].name); 
+      console.log("Song: "+data.tracks.items[i].name); 
+      console.log("URL: "+data.tracks.items[i].href); 
+      console.log("Album: "+data.tracks.items[i].album.name); 
+      console.log("<------------------->"); 
+      }
     });
 
   break;
@@ -42,23 +52,34 @@ switch(command){
 
     axios.get("http://www.omdbapi.com/?t="+request+"&y=&plot=short&apikey=trilogy").then(
       function(response) {
-        console.log("The movie's rating is: " + response.data.imdbRating);
+        console.log(" Title: " + response.data.Title);
+        console.log(" Year: " + response.data.Year);
+        console.log(" IMDB Rating: " + response.data.Ratings[0].Value);
+        console.log(" Roten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log(" Country: " + response.data.Country);
+        console.log(" Language: " + response.data.Language);
+        console.log(" Plot: " + response.data.Plot);
+        console.log(" Director: " + response.data.Director);
+        console.log(" Actors: " + response.data.Actors);
+        console.log("<---------------------------------->")
       }
     );
 
   break;
   case "do-what-it-says":
+   fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+      var random = data.split(',');
+      command = random[0];
+      request = random[1];
+      run();
+    });
 
 
   break;
-
 }
+};
 
-
-
-
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  }
-);
+run()
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # Liri-App
+
+Liri its an app that runs on terminal. it has four different functions.
+
+here is how they work:
+
+1. `node liri.js concert-this <artist/band name here>`
+
+   * This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+2. `node liri.js spotify-this-song '<song name here>'`
+
+   * This will show the following information about the song in your terminal/bash window
+
+     * Artist(s)
+
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+
+
+   * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
+
+   * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
+
+   * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
+
+   * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
+
+   * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
+
+3. `node liri.js movie-this '<movie name here>'`
+
+   * This will output the following information to your terminal/bash window:
+
+     ```
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+     ```
+
+
+
+4. `node liri.js do-what-it-says`
+
+   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+     * Edit the text in random.txt to test out the feature for movie-this and concert-this.
